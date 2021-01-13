c1463285eda9a8944c2560b503fb5e17cfc229b6
alanmartinc <alancabot.ac@gmail.com>
2021-01-12 22:45:29 -0300

    Se agrega ejercicios del capitulo 6

---
+++ capitulo6.js
@@ -0,0 +1,21 @@
+/* 
+DOM: document object model
+Es una interfaz que contiene todos los objetos estandares que nos permiten representar un documento html,xml o xhtml.
+*/ 
+
+// Nodo: Un nodo en el DOM es cualquier etiqueta del cuerpo, como un párrafo, el mismo body o incluso las etiquetas de una lista.
+
+
+// Document: El nodo document es el nodo raiz, a partir del cual derivan el resto de nodos.
+
+
+// Element: Nodos definidos por etiquetas HTML.
+
+
+// Text: El texto dentro de un nodo element se considera un nuevo nodo hijo de tipo text (texto).
+
+
+// Attribute: Los atributos de las etiquetas definen nodos, (En JavaScript no los veremos como nodos, sino como información asociada al nodo de tipo element).
+
+
+// Comentarios y otros: Los comentarios y otros elementos como las declaraciones doctype en cabecera de los documentos HTML generan nodos.
